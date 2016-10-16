09f87d06002274eee3043c513c728d7861667bfb
waterfallrapgame <nautiouscat@gmail.com>
2016-10-16 13:36:52 -0600

    View BT-LE
    Adding the ability to view devices running on bluetooth LE

--- app/src/main/java/com/example/kat/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kat/myapplication/MainActivity.java
@@ -1,43 +1,208 @@
 package com.example.kat.myapplication;
 
 import android.Manifest;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ScrollView;
 
-import static android.Manifest.permission.BLUETOOTH;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
-    Button buttonToggle;
-    private BluetoothAdapter BA;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+    //The Following just
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    public static final int REQUEST_ENABLE_BT = 1;
+
+    private HashMap<String, BTLE_Device> mBTDevicesHashMap;
+    private ArrayList<BTLE_Device> mBTDevicesArrayList;
+    private ListAdapter_BTLE_Devices adapter;
+
+    private Button btn_Scan;
+
+    //private BroadcastReceiver_BTState mBTStateUpdateReceiver;
+    private Scanner_BTLE mBTLeScanner;
+
+
+    int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = {Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BA = BluetoothAdapter.getDefaultAdapter();
-        buttonToggle = (Button) findViewById(R.id.toggleButton);
-        buttonToggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        if(!hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Utils.toast(getApplicationContext(), "BLE not supported");
+            finish();
+        }
 
-                Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(turnOn, 0);
+        //mBTStateUpdateReceiver = new BroadcastReceiver_BTState(getApplicationContext());
+        mBTLeScanner = new Scanner_BTLE(this, 7500, -75);
 
+        mBTDevicesHashMap = new HashMap<>();
+        mBTDevicesArrayList = new ArrayList<>();
+
+        adapter = new ListAdapter_BTLE_Devices(this, R.layout.btle_device_list_item, mBTDevicesArrayList);
+         // = findViewById(R.layout.btle_device_list_item,mBTDevicesArrayList);
+
+
+        
+        ListView listView = new ListView(this);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+        btn_Scan = (Button) findViewById(R.id.btn_scan);
+        ((ScrollView) findViewById(R.id.scrollView)).addView(listView);
+        findViewById(R.id.btn_scan).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //registerReceiver(mBTStateUpdateReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        stopScan();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+       // unregisterReceiver(mBTStateUpdateReceiver);
+        stopScan();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // Check which request we're responding to
+        if (requestCode == REQUEST_ENABLE_BT) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Utils.toast(getApplicationContext(), "Thank you for turning on Bluetooth");
             }
-        });
+            else if (resultCode == RESULT_CANCELED) {
+                Utils.toast(getApplicationContext(), "Please turn on Bluetooth");
+            }
+        }
+    }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btn_scan:
+                Utils.toast(getApplicationContext(), "Scan Button Pressed");
+
+                if (!mBTLeScanner.isScanning()) {
+                    startScan();
+                }
+                else {
+                    stopScan();
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void addDevice(BluetoothDevice device, int rssi) {
+
+        String address = device.getAddress();
+        if (!mBTDevicesHashMap.containsKey(address)) {
+
+            BTLE_Device btleDevice = new BTLE_Device(device);
+            btleDevice.setRSSI(rssi);
+
+
+
+            mBTDevicesHashMap.put(address, btleDevice);
+            mBTDevicesArrayList.add(btleDevice);
+
+        }
+        else {
+            mBTDevicesHashMap.get(address).setRSSI(rssi);
+        }
+
+        adapter.notifyDataSetChanged();
+    }
+
+    public void startScan(){
+        btn_Scan.setText("Scanning...");
+
+        mBTDevicesArrayList.clear();
+        mBTDevicesHashMap.clear();
+
+        adapter.notifyDataSetChanged();
+
+        mBTLeScanner.start();
+    }
+
+    /**
+     * Stops Scanner_BTLE
+     * Changes the scan button text.
+     */
+    public void stopScan() {
+        btn_Scan.setText("Scan Again");
+
+        mBTLeScanner.stop();
     }
 
 
 
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
+    //Just so there is no error at the top
+    //Truthfully this could have been avoided with some other more experienced approach
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-
-
-
+    }
 }
